aea3fbe761174c845125f491406238ba2866e414
ZasZuhaer <39369311+ZasZuhaer@users.noreply.github.com>
2025-06-01 07:12:01 +0600

    Updated BookingCard.tsx

--- src/components/bookings/BookingCard.tsx
+++ src/components/bookings/BookingCard.tsx
@@ -209,7 +209,7 @@ const BookingCard: React.FC<BookingCardProps> = ({
        
 
     <div className="flex flex-col gap-1">
-      <div className="flex items-center gap-1 text-lg font-semibold">
+      <div className="flex items-center gap-1 text-md font-semibold">
         <Calendar className="w-4 h-4 text-gray-400" /> 
         <span>{formatDate(booking.bookingDate)} ({booking.durationDays} days)</span>
       </div>
