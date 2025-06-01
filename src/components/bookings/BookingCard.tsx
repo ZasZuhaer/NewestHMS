113c47f095add5e0d770719ec95a0415819dad5a
ZasZuhaer <39369311+ZasZuhaer@users.noreply.github.com>
2025-06-01 12:33:00 +0600

    Updated BookingCard.tsx

--- src/components/bookings/BookingCard.tsx
+++ src/components/bookings/BookingCard.tsx
@@ -267,7 +267,7 @@ const BookingCard: React.FC<BookingCardProps> = ({
       {booking.checkInDateTime && !booking.checkOutDateTime && (
         <>
           <button onClick={() => setShowExtendModal(true)} className="btn btn-secondary w-40">Extend</button>
-          <button onClick={handleCheckOut} className="btn btn-primary bg-blue-600 w-40">Check Out</button>
+          <button onClick={handleCheckOut} className="btn btn-primary bg-blue-600 w-30">Check Out</button>
         </>
       )}
 
