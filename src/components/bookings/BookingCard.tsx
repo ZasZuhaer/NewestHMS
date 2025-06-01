f35c8e1b596dbcd475aea6e55a15f69eb2169df0
ZasZuhaer <39369311+ZasZuhaer@users.noreply.github.com>
2025-06-01 12:40:35 +0600

    Updated BookingCard.tsx

--- src/components/bookings/BookingCard.tsx
+++ src/components/bookings/BookingCard.tsx
@@ -243,7 +243,7 @@ const BookingCard: React.FC<BookingCardProps> = ({
 
           {cancellationRequest?.status === 'pending' && (
         <div className="flex items-center justify-center bg-amber-100 text-amber-800 px-1 py-1 rounded-md text-xs">
-          Request pending
+          Cancellation request pending
         </div> 
       )}
             
