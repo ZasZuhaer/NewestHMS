8482bbac0953bca286fd3e6ac3e993b63d7ce67d
ZasZuhaer <39369311+ZasZuhaer@users.noreply.github.com>
2025-06-01 07:56:08 +0600

    Updated BookingCard.tsx

--- src/components/bookings/BookingCard.tsx
+++ src/components/bookings/BookingCard.tsx
@@ -217,7 +217,7 @@ const BookingCard: React.FC<BookingCardProps> = ({
 
     <div className="flex flex-col gap-1">
 
-      <div className="flex items-center gap-1 text-sm text-gray-500">
+      <div className="flex items-center gap-1 text-lg text-gray-500">
   <Users className="w-5 h-5 text-gray-400" />
   <span>{booking.numberOfPeople} {booking.numberOfPeople > 1 ? 'Guests' : 'Guest'}</span>
 </div>
