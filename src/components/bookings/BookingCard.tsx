c0806904215c94ac836f2296f45ef8f061fb47e0
ZasZuhaer <39369311+ZasZuhaer@users.noreply.github.com>
2025-07-11 01:19:24 +0600

    Updated BookingCard.tsx

--- src/components/bookings/BookingCard.tsx
+++ src/components/bookings/BookingCard.tsx
@@ -189,7 +189,7 @@ const BookingCard: React.FC<BookingCardProps> = ({
       ? 'bg-green-300 border-green-300'
       : booking.checkInDateTime
       ? 'bg-red-300 border-red-300'
-      : 'bg-yellow-50 border-yellow-300'
+      : 'bg-yellow-300 border-yellow-300'
   } ${variant === 'list' ? 'p-2' : 'p-3'}`}
 >
 
