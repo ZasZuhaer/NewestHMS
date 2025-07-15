bd92af144b8d6256993fd9827d060b638fd014ed
ZasZuhaer <39369311+ZasZuhaer@users.noreply.github.com>
2025-07-16 04:23:26 +0600

    Updated RoomFilters.tsx

--- src/components/rooms/RoomFilters.tsx
+++ src/components/rooms/RoomFilters.tsx
@@ -115,7 +115,7 @@ const RoomFilters: React.FC<RoomFiltersProps> = ({ onFilterChange }) => {
         
         <div className="flex items-end space-x-4">
 
-          <div>
+          <div className="flex items-center h-[38px] ">
             <input
               type="checkbox"
               id="hasAC"
