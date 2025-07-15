c808b80988c3853cbbec8ea3c72234778bfd368b
ZasZuhaer <39369311+ZasZuhaer@users.noreply.github.com>
2025-07-16 04:25:13 +0600

    Updated RoomFilters.tsx

--- src/components/rooms/RoomFilters.tsx
+++ src/components/rooms/RoomFilters.tsx
@@ -130,7 +130,7 @@ const RoomFilters: React.FC<RoomFiltersProps> = ({ onFilterChange }) => {
           
           <button
             type="submit"
-            className="w-full md:flex-1 inline-flex items-center justify-center px-2 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-teal-600 hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-500"
+            className="w-full md:flex-1 inline-flex items-center justify-center px-2 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-teal-600 hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-500" 
           >
             <Search className="h-4 w-4 mr-2" />
             Search
